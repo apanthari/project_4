25a7a99b5b21de77952a32ed84e70fdce1cd1a8d
rikinp22 <rikinp22@gmail.com>
2023-05-20 14:29:44 -0700

    Added new images to website

---
+++ musicmania/music.js
@@ -0,0 +1,126 @@
+// Fetch the CSV file
+fetch('song_data.csv')
+  .then(response => response.text())
+  .then(csvData => {
+    // Convert CSV to JSON
+    const jsonData = csvToJSON(csvData);
+    
+    // Extract only the "song_name" column from the JSON data
+    const songNames = jsonData.map(row => row.song_name);
+    
+    // Display the song names
+    displaySearchResults(songNames);
+    
+    // Handle search input changes
+    const searchInput = document.getElementById('searchInput');
+
+    searchInput.addEventListener('input', function () {
+      const searchTerm = this.value.toLowerCase();
+      const filteredData = songNames.filter(name =>
+        name.toLowerCase().includes(searchTerm)
+      );
+
+      displaySearchResults(filteredData);
+    });
+  });
+
+
+// // Fetch the CSV file
+// fetch('song_data.csv')
+//   .then(response => response.text())
+//   .then(csvData => {
+//     // Convert CSV to JSON
+//     const jsonData = csvToJSON(csvData);
+    
+//     // Sort the JSON data based on song popularity (assuming it is a numeric column)
+//     jsonData.sort((a, b) => b.song_popularity - a.song_popularity);
+    
+//     // Display the top 10 songs
+//     displayTop10Results(jsonData);
+    
+//     // Handle search input changes
+//     const searchInput = document.getElementById('searchInput');
+
+//     searchInput.addEventListener('input', function () {
+//       const searchTerm = this.value.toLowerCase();
+//       const filteredData = jsonData.filter(row =>
+//         Object.values(row).some(value =>
+//           value.toLowerCase().includes(searchTerm)
+//         )
+//       );
+
+//       displaySearchResults(filteredData);
+//     });
+//   });
+
+// // Fetch the CSV file
+// fetch('song_data.csv')
+//   .then(response => response.text())
+//   .then(csvData => {
+//     // Convert CSV to JSON
+//     const jsonData = csvToJSON(csvData);
+    
+//     // Handle search input changes
+//     const searchInput = document.getElementById('searchInput');
+//     const resultsTable = document.getElementById('resultsTable');
+
+//     searchInput.addEventListener('input', function () {
+//       const searchTerm = this.value.toLowerCase();
+//       const filteredData = jsonData.filter(row =>
+//         Object.values(row).some(value =>
+//           value.toLowerCase().includes(searchTerm)
+//         )
+//       );
+
+//       displaySearchResults(filteredData);
+//     });
+//   });
+
+// // Convert CSV to JSON
+// function csvToJSON(csvData) {
+//   const lines = csvData.split('\n');
+//   const headers = lines[0].split(',');
+//   const jsonData = [];
+
+//   for (let i = 1; i < lines.length; i++) {
+//     const currentLine = lines[i].split(',');
+//     if (currentLine.length !== headers.length) continue;
+
+//     const row = {};
+//     for (let j = 0; j < headers.length; j++) {
+//       row[headers[j]] = currentLine[j];
+//     }
+
+//     jsonData.push(row);
+//   }
+
+//   return jsonData;
+// }
+
+// // Display search results in a table
+// function displaySearchResults(results) {
+//   const resultsTable = document.getElementById('resultsTable');
+
+//   // Clear previous search results
+//   resultsTable.innerHTML = '';
+
+//   // Create table headers
+//   const headerRow = document.createElement('tr');
+//   for (let key in results[0]) {
+//     const headerCell = document.createElement('th');
+//     headerCell.textContent = key;
+//     headerRow.appendChild(headerCell);
+//   }
+//   resultsTable.appendChild(headerRow);
+
+//   // Create table rows for search results
+//   results.forEach(result => {
+//     const row = document.createElement('tr');
+//     for (let key in result) {
+//       const cell = document.createElement('td');
+//       cell.textContent = result[key];
+//       row.appendChild(cell);
+//     }
+//     resultsTable.appendChild(row);
+//   });
+// }
